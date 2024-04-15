9d9fed10a9918fe9292ca02c5fb748eed4b96dce
GulhermeC <gjscraveiro@ua.pt>
2024-04-15 12:49:32 +0100

    i2c temperature module

---
+++ i2c_temp/run.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "driver/i2c_master.h"
+
+void app_main (void)
+{
+	i2c_master_bus_config_t i2cMasterCfg = {
+		.clk_source = I2C_CLK_SRC_DEFAULT,
+		.i2c_port = I2C_NUM_0,
+		.scl_io_num = 0,
+		.sda_io_num = 1,
+		.glitch_ignore_cnt = 7,
+		.flags.enable_internal_pullup = true,
+	};
+
+	i2c_master_bus_handle_t i2cBusHandle;
+	ESP_ERROR_CHECK(i2c_new_master_bus(&i2cMasterCfg, &i2cBusHandle));
+
+	i2c_device_config_t dev_cfg = {
+		.dev_addr_length = I2C_ADDR_BIT_LEN_7,
+		.device_address = 0x49,
+		.scl_speed_hz = 50000,
+	};
+
+	i2c_master_dev_handle_t i2cDevHandle;
+	ESP_ERROR_CHECK(i2c_master_bus_add_device(i2cBusHandle, &dev_cfg, &i2cDevHandle));
+
+
+	while (1)
+	{
+		uint8_t buffer[2] = {0x01, 0x80};
+		//uint8_t buffer[2] = {0x01, 0x00};
+		ESP_ERROR_CHECK(i2c_master_transmit(i2cDevHandle, buffer, sizeof(buffer), -1));
+		vTaskDelay(10);
+	}
+}
---
+++ timer/esp_timer_example_main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "esp_timer.h"
+#include "esp_log.h"
+#include "esp_sleep.h"
+#include "sdkconfig.h"
+
+static void periodic_timer_callback(void* arg);
+
+static const char* TAG = "example";
+
+void app_main(void)
+{
+    /* Create timer:
+     * periodic timer which will run every 0.5s, and print a message
+     */
+
+    const esp_timer_create_args_t periodic_timer_args = {
+            .callback = &periodic_timer_callback,
+            /* name is optional, but may help identify the timer when debugging */
+            .name = "periodic"
+    };
+
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    /* The timer has been created but is not running yet */
+
+    /* Start the timers */
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 500000));
+    ESP_LOGI(TAG, "Started timers, time since boot: %lld us", esp_timer_get_time());
+
+    /* Print debugging information about timers to console every 2 seconds */
+    for (int i = 0; i < 5; ++i) {
+        ESP_ERROR_CHECK(esp_timer_dump(stdout));
+        usleep(2000000);
+    }
+
+    /* Timekeeping continues in light sleep, and timers are scheduled
+     * correctly after light sleep.
+     */
+    int64_t t1 = esp_timer_get_time();
+    ESP_LOGI(TAG, "Entering light sleep for 0.5s, time since boot: %lld us", t1);
+
+    ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(500000));
+    esp_light_sleep_start();
+
+    int64_t t2 = esp_timer_get_time();
+    ESP_LOGI(TAG, "Woke up from light sleep, time since boot: %lld us", t2);
+
+    assert(llabs((t2 - t1) - 500000) < 1000);
+
+    /* Let the timer run for a little bit more */
+    usleep(2000000);
+
+    /* Clean up and finish the example */
+    ESP_ERROR_CHECK(esp_timer_stop(periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_delete(periodic_timer));
+    ESP_LOGI(TAG, "Stopped and deleted timers");
+}
+
+static void periodic_timer_callback(void* arg)
+{
+    int64_t time_since_boot = esp_timer_get_time();
+    ESP_LOGI(TAG, "Periodic timer called, time since boot: %lld us", time_since_boot);
+}
